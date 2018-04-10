2c6eee125edc05021250882e8ae9cfe6b7cc15c2
Fischaela <michaela.lehr@geildanke.com>
2018-04-10 17:11:35 +0200

    ADD sound

---
+++ components/sound.js
@@ -0,0 +1,26 @@
+class Sound {
+  constructor(context) {
+    this.context = context;
+  }
+
+  setup() {
+    this.oscillator = this.context.createOscillator();
+    this.gainNode = this.context.createGain();
+    this.oscillator.connect(this.gainNode);
+    this.gainNode.connect(this.context.destination);
+    this.oscillator.type = 'sine';
+  }
+
+  play(value) {
+    this.setup();
+    this.oscillator.frequency.value = value;
+    this.gainNode.gain.setValueAtTime(1, this.context.currentTime);
+    this.oscillator.start(this.context.currentTime);
+    this.stop(this.context.currentTime);
+  }
+
+  stop() {
+    this.gainNode.gain.exponentialRampToValueAtTime(0.001, this.context.currentTime + 1);
+    this.oscillator.stop(this.context.currentTime + 1);
+  }
+}
--- components/xylophone.js
+++ components/xylophone.js
@@ -13,13 +13,28 @@ AFRAME.registerComponent('xylophone', {
       'royalblue',
       'mediumslateblue'
     ];
+    const frequencies = [
+      261.63,
+      293.66,
+      329.63,
+      349.23,
+      392.00,
+      440.00,
+      493.88,
+      523.25
+    ];
+
     let barGroup = document.createElement('a-entity');
     barGroup.setAttribute('position', this.data.position);
+
+    let context = new (window.AudioContext || window.webkitAudioContext)();
 
     for (let i = 0, iMax = 8; i < iMax; i += 1) {
       let bar = document.createElement('a-box');
       let positionX = i * 0.055;
       let depth = 0.2 - (i * 0.01);
+      let sound = new Sound(context);
+      let frequency = frequencies[i];
 
       bar.setAttribute('color', colors[i]);
       bar.setAttribute('shadow', 'cast: true; receive: false');
@@ -28,6 +43,10 @@ AFRAME.registerComponent('xylophone', {
       bar.setAttribute('height', 0.01);
       bar.setAttribute('position', positionX + ' 0 0' );
       bar.setAttribute('animation__anim-hit', 'property: scale; startEvents: mousedown; from: 1 1 1; to: 0.95 0.95 0.95; dur: 100; easing: linear; loop: 2; dir: alternate');
+      bar.addEventListener('mousedown', function() {
+        sound.play(frequency);
+        sound.stop();
+      });
       barGroup.append(bar);
     }
 
