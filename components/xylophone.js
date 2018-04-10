d99ac2213e36760ce11775dec6cb32eeea126f90
Fischaela <michaela.lehr@geildanke.com>
2018-04-10 16:45:15 +0200

    MOD bargroup and bar properties

--- components/xylophone.js
+++ components/xylophone.js
@@ -14,10 +14,19 @@ AFRAME.registerComponent('xylophone', {
       'mediumslateblue'
     ];
     let barGroup = document.createElement('a-entity');
-    barGroup.setAttribute('position', this.data.position);
+    barGroup.object3D.el.position = this.data.position;
 
     for (let i = 0, iMax = 8; i < iMax; i += 1) {
-      let bar = document.createElement('a-box')
+      let bar = document.createElement('a-box');
+      let positionX = i * 0.055;
+      let depth = 0.2 - (i * 0.01);
+
+      bar.setAttribute('color', colors[i]);
+      bar.setAttribute('shadow', 'cast: true; receive: false');
+      bar.setAttribute('depth', depth);
+      bar.setAttribute('width', 0.05);
+      bar.setAttribute('height', 0.01);
+      bar.object3D.el.position =positionX + ' 0 0';
       barGroup.append(bar);
     }
 
